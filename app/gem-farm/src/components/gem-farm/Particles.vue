638dc66545ff3ab07d504f5fb8eaac124d3daf6f
vYYnc <vincent@maaany.de>
2022-03-17 00:49:17 +0100

    style changes

---
+++ app/gem-farm/src/components/gem-farm/Particles.vue
@@ -0,0 +1,14 @@
+<template>
+<canvas id="myCanvas"></canvas>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+<script async="" defer="" src="https://buttons.github.io/buttons.js"></script>
+
+<style lang="scss" scoped>
+
+</style>
